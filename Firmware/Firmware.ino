68b6b89b0f58db82da5f1606fdd720c23e920a9f
Matt Mets <matt.mets@cibomahto.com>
2013-03-30 01:28:13 +0800

    Tweak speed, add TODOs. Still not ready for prime time- data rate is too low.

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -3,6 +3,10 @@
 
 #define PIN_RS485_DE     3
 
+
+#define NUMBER_OF_CHANNELS 16
+uint16_t testValues[NUMBER_OF_CHANNELS];
+
 void setup() {
   pinMode(PIN_LED_WHITE, OUTPUT);
   pinMode(PIN_LED_BLUE, OUTPUT);
@@ -15,14 +19,35 @@ void setup() {
   pinMode(PIN_RS485_DE, OUTPUT);
   digitalWrite(PIN_RS485_DE, HIGH);
   
-  Serial1.begin(38400);
+  Serial1.begin(230400);
+  
+  // Preset the test values
+  for(int i = 0; i < NUMBER_OF_CHANNELS; i++) {
+    testValues[i] = i*(65535.0/NUMBER_OF_CHANNELS);
+  }
 }
 
+void sendTestFrame() {
+  Serial1.write(0xde);
+  Serial1.write(0xad);
+  Serial1.write(NUMBER_OF_CHANNELS*2);
+  for(int i = 0; i < NUMBER_OF_CHANNELS; i++) {
+    Serial1.write(testValues[i] & 0xFF);        // low byte
+    Serial1.write((testValues[i] >> 8) & 0xFF); // high byte
+    
+    testValues[i] += 500;
+  }
+  Serial1.write(0xff);  // TODO: Implement CRC
+}
 
 void loop() {
-  if(Serial.available()) {
-    digitalWrite(PIN_LED_BLUE, 1);
-    Serial1.write(Serial.read());  
-    digitalWrite(PIN_LED_BLUE, 0);
-  }
+//  if(Serial.available()) {
+    while(1) {
+      if (Serial.available()) {
+        Serial1.write(Serial.read());  
+      }
+    }
+//  }
+//  
+//  sendTestFrame();
 }
